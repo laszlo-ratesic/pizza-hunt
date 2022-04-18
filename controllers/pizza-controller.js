e4ede3ce5f5cfc591ba8702fa5c2cc8eda12413f
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-18 14:37:00 -0500

    add update pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -30,12 +30,26 @@ const pizzaController = {
       });
   },
 
-//   CREATE pizza
+    // CREATE pizza
   createPizza({ body }, res) {
       Pizza.create(body)
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.status(400).json(err));
-  }
+  },
+
+    // UPDATE pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id! '});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
 };
+
 
 module.exports = pizzaController;
