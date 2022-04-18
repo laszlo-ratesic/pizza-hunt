a00f9b0197d0c7cbc1be76c7e880f6a6ebb5c81a
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-18 14:39:10 -0500

    implmenent delete

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -48,6 +48,19 @@ const pizzaController = {
             res.json(dbPizzaData);
         })
         .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
     }
 };
 
