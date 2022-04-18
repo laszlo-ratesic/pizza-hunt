2fcc1a3df8b3e7a34a8cdf32da6468ab543e3d85
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-18 14:34:05 -0500

    add create pizza function

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -29,6 +29,13 @@ const pizzaController = {
         res.status(400).json(err);
       });
   },
+
+//   CREATE pizza
+  createPizza({ body }, res) {
+      Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = pizzaController;
