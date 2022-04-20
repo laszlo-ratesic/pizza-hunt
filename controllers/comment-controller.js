3e4d83c93d4a4eb333417ebe89c83e150cc643ef
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-19 21:44:17 -0500

    configure reply routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,23 @@ const commentController = {
       .catch((err) => res.json(err));
   },
 
+  // add reply to a comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -44,6 +61,20 @@ const commentController = {
       })
       .catch((err) => res.json(err));
   },
+
+  // remove a reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      // use MongoDB $pull operator to remove specific reply
+      // from the replies array WHERE the replyId
+      // matches the value of params.replyId
+      { $pull: { replies: {replyId: params.replyId } } },
+      { new: true }
+    )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
+  }
 };
 
 module.exports = commentController;
