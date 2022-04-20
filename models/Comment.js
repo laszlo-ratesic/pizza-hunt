4457b7f58e7d1c0260cc80a20333484c264999ba
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-19 21:10:31 -0500

    create reply schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+const replySchema = new Schema({
+  replyBody: {
+    type: String,
+  },
+  writtenBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
 });
 
-const Comment = model('Comment', CommentSchema);
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String,
+  },
+  commentBody: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = model("Comment", CommentSchema);
 
 module.exports = Comment;
