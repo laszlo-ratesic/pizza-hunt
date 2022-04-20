2df683ec11b52f101bfeadb99bcac57ce12e8218
laszlo-ratesic <keenanrc@hotmail.com>
2022-04-20 09:48:50 -0500

    save record w/ no connection fn

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -13,21 +13,34 @@ request.onupgradeneeded = function (event) {
 };
 
 // upon success
-request.onsuccess = function(event) {
-    // when db is successfully created with its object store
-    // (from onupgradeneeded event above) or established a connection,
-    // save reference to db in global var
-    db.event.target.result;
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store
+  // (from onupgradeneeded event above) or established a connection,
+  // save reference to db in global var
+  db.event.target.result;
 
-    // check if app is online, if yes
-    // run uploadPizza() fn to send all local db data to api
-    if (navigator.onLine) {
-        // we haven't created this yet
-        // uploadPizza();
-    }
+  // check if app is online, if yes
+  // run uploadPizza() fn to send all local db data to api
+  if (navigator.onLine) {
+    // we haven't created this yet
+    // uploadPizza();
+  }
 };
 
-request.onerror = function(event) {
-    // log error
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  // log error
+  console.log(event.target.errorCode);
+};
+
+// This function will execute if we attempt to submit a new pizza
+// and there's no internet connection
+function saveRecord(record) {
+  // open a new txn w/ the db with read and write permissions
+  const txn = db.transaction(["new_pizza"], "readwrite");
+
+  // access the object store for 'new_pizza'
+  const pizzaObjectStore = txn.objectStore("new_pizza");
+
+  // add record to your store with add method
+  pizzaObjectStore.add(record);
 }
